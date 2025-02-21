5f8712f8e191975caa515004feed6e0ca910efef
APIVerve Team <hello@apiverve.com>
2025-02-21 08:18:20 -0600

    Added new API Endpoints

---
+++ ResponseObj.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace APIVerve
+{
+public class data
+{
+    [JsonProperty("word")]
+    public string word { get; set; }
+
+    [JsonProperty("opposites")]
+    public string[] opposites { get; set; }
+
+}
+
+public class ResponseObj
+{
+    [JsonProperty("status")]
+    public string status { get; set; }
+
+    [JsonProperty("error")]
+    public object error { get; set; }
+
+    [JsonProperty("data")]
+    public data data { get; set; }
+
+    [JsonProperty("code")]
+    public int code { get; set; }
+
+}
+
+}
